9f534667ca42e57d54c686d982d2e30764f09570
Oscar Söderlund <poscar@spotify.com>
2014-10-27 21:50:01 +0100

    Add more tests

--- src/cljs/monkey_music/rules.cljs
+++ src/cljs/monkey_music/rules.cljs
@@ -9,46 +9,56 @@
      :album 2
      :song 1
      0))
-  ([item & items] (reduce + (map value-of (conj items item)))))
+  ([item & items]
+   (reduce + (map value-of (conj items item)))))
 
-(defn game-over? [state]
-  (let [all-units-on-map (flatten (states/layout state))
-        all-picked-up-items (mapcat :picked-up-items (states/players state))]
-    (not-any? #{:song :album :playlist} (concat all-units-on-map
-                                                all-picked-up-items))))
-
-(defn can-pick-up-more? [state player-name]
-  (< (count (states/picked-up-items state player-name))
+(defn can-pick-up-more? [state player-id]
+  (< (count (states/picked-up-items state player-id))
      (states/pick-up-limit state)))
 
-(defn move-player* [state player-name direction]
-  (let [player-position (states/position state player-name)
+(defn game-over? [state]
+  (let [all-the-things (concat (states/all-units state)
+                               (states/all-picked-up-items state))
+        total-remaining-points (apply value-of all-the-things)]
+    (or (zero? (states/total-remaining-turns state))
+        (zero? total-remaining-points))))
+
+(defn move-player* [state player-id direction]
+  (let [player-position (states/position state player-id)
         target-position (positions/translate player-position direction)
         unit-at-target-position (states/unit-at state target-position)
-        picked-up-items (states/picked-up-items state player-name)
-        value-of-picked-up-items (apply value-of (states/picked-up-items state player-name))]
+        picked-up-items (states/picked-up-items state player-id)
+        value-of-picked-up-items (apply value-of (states/picked-up-items state player-id))]
     (case unit-at-target-position
       :empty
       (-> state
-          (states/set-position player-name target-position)
+          (states/set-position player-id target-position)
           (states/set-unit-at target-position :monkey)
           (states/set-unit-at player-position :empty))
       (:song :album :playlist)
-      (if (can-pick-up-more? state player-name)
+      (if (can-pick-up-more? state player-id)
         (-> state
-          (states/add-to-picked-up-items player-name unit-at-target-position)
+          (states/add-to-picked-up-items player-id unit-at-target-position)
           (states/set-unit-at target-position :empty))
         state)
       :user
       (-> state
-          (states/inc-score player-name value-of-picked-up-items)
-          (states/set-picked-up-items player-name []))
+          (states/inc-score player-id value-of-picked-up-items)
+          (states/set-picked-up-items player-id []))
       state)))
 
-(defn move-player [state player-name direction]
-  (if (and (pos? (states/remaining-turns state player-name))
+(defn move-player [state player-id direction]
+  (if (and (pos? (states/remaining-turns state player-id))
            (not (game-over? state)))
     (-> state
-        (states/dec-remaining-turns player-name)
-        (move-player* player-name direction))
+        (states/dec-remaining-turns player-id)
+        (move-player* player-id direction))
     state))
+
+(defn state-for-player [state player-id]
+  {:layout (states/layout state)
+   :pick-up-limit (states/pick-up-limit state)
+   :remaining-turns (states/remaining-turns state player-id)
+   :position (states/position state player-id)
+   :score (states/score state player-ids)
+   :picked-up-items (states/picked-up-items state player-id)})
--- src/cljs/monkey_music/states.cljs
+++ src/cljs/monkey_music/states.cljs
@@ -7,17 +7,17 @@
    :turns turns
    :position position})
 
-(defn create-players [player-names player-positions turns]
+(defn create-players [player-ids player-positions turns]
   (let [players (mapv (partial create-player turns) player-positions)]
-    (into {} (map vector player-names players))))
+    (into {} (map vector player-ids players))))
 
 (defn create
-  [player-names
+  [player-ids
    {layout :layout
     pick-up-limit :pick-up-limit
     turns :turns}]
   (let [player-positions (positions/find-players layout)
-        players (create-players player-names player-positions turns)]
+        players (create-players player-ids player-positions turns)]
     {:layout layout
      :players players
      :pick-up-limit pick-up-limit}))
@@ -28,32 +28,34 @@
 
 (def players (comp vals :players))
 
-(defn position [state player-name]
-  (get-in state [:players player-name :position]))
+(def player-ids (comp keys :players))
 
-(defn set-position [state player-name position]
-  (assoc-in state [:players player-name :position] position))
+(defn position [state player-id]
+  (get-in state [:players player-id :position]))
 
-(defn score [state player-name]
-  (get-in state [:players player-name :score]))
+(defn set-position [state player-id position]
+  (assoc-in state [:players player-id :position] position))
 
-(defn inc-score [state player-name amount]
-  (update-in state [:players player-name :score] + amount))
+(defn score [state player-id]
+  (get-in state [:players player-id :score]))
 
-(defn remaining-turns [state player-name]
-  (get-in state [:players player-name :turns]))
+(defn inc-score [state player-id amount]
+  (update-in state [:players player-id :score] + amount))
 
-(defn dec-remaining-turns [state player-name]
-  (update-in state [:players player-name :turns] dec))
+(defn remaining-turns [state player-id]
+  (get-in state [:players player-id :turns]))
 
-(defn picked-up-items [state player-name]
-  (get-in state [:players player-name :picked-up]))
+(defn dec-remaining-turns [state player-id]
+  (update-in state [:players player-id :turns] dec))
 
-(defn set-picked-up-items [state player-name items]
-  (assoc-in state [:players player-name :picked-up] items))
+(defn picked-up-items [state player-id]
+  (get-in state [:players player-id :picked-up]))
 
-(defn add-to-picked-up-items [state player-name item]
-  (update-in state [:players player-name :picked-up] conj item))
+(defn set-picked-up-items [state player-id items]
+  (assoc-in state [:players player-id :picked-up] items))
+
+(defn add-to-picked-up-items [state player-id item]
+  (update-in state [:players player-id :picked-up] conj item))
 
 (defn unit-at [state position]
   (get-in state (into [:layout] position) :out-of-bounds))
@@ -61,10 +63,13 @@
 (defn set-unit-at [state position unit]
   (assoc-in state (into [:layout] position) unit))
 
-(defn for-player [state player-name]
-  {:layout (:layout state)
-   :pick-up-limit (:pick-up-limit state)
-   :remaining-turns (remaining-turns state player-name)
-   :position (position state player-name)
-   :score (score state player-name)
-   :picked-up-items (picked-up-items state player-name)})
+;; Derived
+
+(defn all-units [state]
+  (flatten (layout state)))
+
+(defn all-picked-up-items [state]
+  (mapcat (partial picked-up-items state) (player-ids state)))
+
+(defn total-remaining-turns [state]
+  (reduce + (map (partial remaining-turns) (player-ids state))))
