84b29575176e8a5b40e7b9966a0164269683578c
Oscar Söderlund <poscar@spotify.com>
2014-11-21 14:52:52 +0100

    Add skeleton bananas test

---
+++ test/monkey_music/bananas_test.cljs
@@ -0,0 +1,6 @@
+(ns monkey-music.bananas-test
+  (:require-macros [cemerick.cljs.test :refer (deftest is testing)])
+  (:require [cemerick.cljs.test :refer (test-ns)]
+            [monkey-music.core :as c]))
+
+(test-ns 'monkey-music.bananas-test)
